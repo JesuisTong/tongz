d37991adb1422766acc68af27e01b3142408d2fa
TongZ <iamtongton@gmail.com>
2022-06-06 11:49:10 +0800

    feat: add two articles

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,10 +2,10 @@ import path from "path"
 
 module.exports = {
     siteMetadata: {
-        title: `Gatsby Default Starter`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `@gatsbyjs`,
-        siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+        title: `TongZ`,
+        description: `TongZ's personal website`,
+        author: `iamtongton@gmail.com`,
+        siteUrl: `https://tongz.gatsbyjs.io/`,
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
